1f91afaf6185847a30465055ecc2e9ccf7aac996
OnodrimOfTheNorth <152860809+OnodrimOfTheNorth@users.noreply.github.com>
2023-12-04 02:08:37 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // You can implement any required JavaScript functionality here, such as:
+    // - Dynamic load projects
+    // - Form submission handling
+    // - Responsive navbar toggle
+});
